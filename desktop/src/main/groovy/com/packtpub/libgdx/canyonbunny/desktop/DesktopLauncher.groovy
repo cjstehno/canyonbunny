a0ded094d31c20693dc90b721f93790b5d4a6cec
cjstehno <chris@stehno.com>
2015-11-10 19:01:29 -0600

    Moved the image packing out of the game startup code and into the build - sanity at last. Everything seems to be working now.

--- desktop/src/main/groovy/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.groovy
+++ desktop/src/main/groovy/com/packtpub/libgdx/canyonbunny/desktop/DesktopLauncher.groovy
@@ -1,26 +1,11 @@
 package com.packtpub.libgdx.canyonbunny.desktop
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration
-import com.badlogic.gdx.tools.texturepacker.TexturePacker
 import com.packtpub.libgdx.canyonbunny.CanyonBunnyMain
-
 
 public class DesktopLauncher {
 
-    private static boolean rebuildAtlas = false
-    private static drawDebugOutline = true
-
     static void main(arg) {
-        if( rebuildAtlas ){
-            TexturePacker.Settings settings = new TexturePacker.Settings()
-            settings.maxWidth = 1024
-            settings.maxHeight = 1024
-            settings.duplicatePadding = false
-            settings.debug = drawDebugOutline
-
-            TexturePacker.process(settings, '../raw-assets/images', '../src/resources/images' , 'canyonbunny.pack')
-        }
-
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration()
         config.width = 800
         config.height = 480
